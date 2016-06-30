5b58e7efc18abf5296e7e274d707c98f73e47f22
Patrick Balestra <me@patrickbalestra.com>
2016-06-30 15:14:24 +0200

    Improve code and fix bug when using a return type

--- CleanClosure/SourceEditorCommand.swift
+++ CleanClosure/SourceEditorCommand.swift
@@ -10,25 +10,31 @@ import Foundation
 import XcodeKit
 
 extension NSString {
-    func remove(character: String, in range: NSRange) -> NSString {
-        return self.replacingOccurrences(of: character, with: "", options: .caseInsensitiveSearch, range: range)
+    // Remove the given chracters in the range from the string.
+    func remove(characters: [Character], in range: NSRange) -> NSString {
+        var cleanedString = self
+        for char in characters {
+            cleanedString = cleanedString.replacingOccurrences(of: String(char), with: "", options: .caseInsensitiveSearch, range: range)
+        }
+        return cleanedString
     }
 }
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void {
-        
         var updatedLineIndexes = [Int]()
         
+        // Find lines that contain a closure syntax
         for lineIndex in 0 ..< invocation.buffer.lines.count {
             let line = invocation.buffer.lines[lineIndex] as! NSString
             do {
-                let regex = try RegularExpression(pattern: "\\{[\\s]*\\([a-zA-Z0-9_\\-,\\s]+\\)[\\s]*in", options: .caseInsensitive)
+                let regex = try RegularExpression(pattern: "\\{.*\\(.+\\).+in", options: .caseInsensitive)
                 let range = NSRange(location: 0, length: line.length)
                 let results = regex.matches(in: line as String, options: .reportProgress, range: range)
-                for result in results {
-                    let cleanLine = line.remove(character: "(", in: result.range).remove(character: ")", in: result.range)
+                // When a closure is found, clean up its syntax
+                _ = results.map { result in
+                    let cleanLine = line.remove(characters: ["(", ")"], in: result.range)
                     updatedLineIndexes.append(lineIndex)
                     invocation.buffer.lines[lineIndex] = cleanLine
                 }
@@ -36,15 +42,15 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                 completionHandler(error as NSError)
             }
         }
-
-        let updatedSelections: [XCSourceTextRange] = updatedLineIndexes.map { lineIndex in
-            let lineSelection = XCSourceTextRange()
-            lineSelection.start = XCSourceTextPosition(line: lineIndex, column: 0)
-            lineSelection.end = XCSourceTextPosition(line: lineIndex, column: 0)
-            return lineSelection
-        }
         
-        if updatedSelections.count > 0 {
+        // If at least a line was changed, create an array of changes and pass it to the buffer selections.
+        if updatedLineIndexes.count > 0 {
+            let updatedSelections: [XCSourceTextRange] = updatedLineIndexes.map { lineIndex in
+                let lineSelection = XCSourceTextRange()
+                lineSelection.start = XCSourceTextPosition(line: lineIndex, column: 0)
+                lineSelection.end = XCSourceTextPosition(line: lineIndex, column: 0)
+                return lineSelection
+            }
             invocation.buffer.selections.setArray(updatedSelections)
         }
         
